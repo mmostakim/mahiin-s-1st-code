055ecbb6f42906441dde86d975de56f8ed0dff1b
mmostakim <mahinbhai023@gmail.com>
2023-10-18 20:13:50 +0600

    first commit

--- crud/gittry.php
+++ crud/gittry.php
@@ -6,7 +6,6 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello Mahin Bhai</h1>
-    <P> Hello World</P>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis quam perferendis tempore qui quibusdam beatae, veniam vero atque vitae rem odit, deserunt corrupti et perspiciatis sequi fugiat nemo sunt! At quae architecto vel est illo. Quia a odit fugit error sapiente. Architecto facere in aut quaerat mollitia unde quam velit. Quos vitae ullam modi facere nihil beatae, officiis quasi autem necessitatibus? Error soluta reiciendis repudiandae saepe vel similique sint fugit quas, possimus illum architecto voluptatibus illo distinctio. Eligendi soluta et deserunt. Id nihil recusandae error perferendis rerum minus doloribus quod, magnam voluptatibus natus, tenetur consequuntur aperiam accusantium suscipit laboriosam obcaecati.</p>
 </body>
 </html>
